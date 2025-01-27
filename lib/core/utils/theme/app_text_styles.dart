087d0843dbed8ab08c623ab412d87c0cf2c4ef2b
Ali gad <aligad.17611@gmail.com>
2025-01-27 15:18:10 +0200

    update some styles

--- lib/core/utils/theme/app_text_styles.dart
+++ lib/core/utils/theme/app_text_styles.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class AppTextStyles {
-  static const TextStyle textStyle24 = TextStyle(
-    fontSize: 24,
+  static  TextStyle textStyle24 = TextStyle(
+    fontSize: 24.sp,
     fontWeight: FontWeight.w400,
   );
 
-  static const TextStyle textStyle32 = TextStyle(
-    fontSize: 32,
+  static  TextStyle textStyle32 = TextStyle(
+    fontSize: 32.sp,
     fontWeight: FontWeight.w400,
   );
 
-  static const TextStyle textStyle40 = TextStyle(
-    fontSize: 40,
+  static  TextStyle textStyle40 = TextStyle(
+    fontSize: 40.sp,
     fontWeight: FontWeight.w400,
   );
 
