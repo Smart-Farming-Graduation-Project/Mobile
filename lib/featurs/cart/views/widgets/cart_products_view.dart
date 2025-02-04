ba382929b3ea0080ffd23d759dd6470d1d1b750f
Ali gad <aligad.17611@gmail.com>
2025-02-04 05:14:38 +0200

    update cart cubit functions

--- lib/featurs/cart/views/widgets/cart_products_view.dart
+++ lib/featurs/cart/views/widgets/cart_products_view.dart
@@ -1,34 +1,39 @@
-
-import 'package:crop_guard/featurs/cart/manger/models/cart_product.dart';
+import 'package:crop_guard/featurs/cart/manger/cubit/cart_cubit.dart';
+import 'package:crop_guard/featurs/cart/manger/cubit/cart_state.dart';
 import 'package:crop_guard/featurs/cart/views/widgets/cart_item_widget.dart';
 import 'package:crop_guard/featurs/cart/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CartProductsView extends StatelessWidget {
-  const CartProductsView({
-    super.key,
-  });
+  const CartProductsView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:  AppBar(
+      appBar: AppBar(
         title: const CustomAppBar(),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: cartProducts.length,
-              itemBuilder: (context, index) => CartItemWidget(
-                cartItem: cartProducts[index],
-              ),
-            ),
-          ),
-          // CheckoutSummary(),
-      
-        ],
+      body: BlocBuilder<CartCubit, CartState>(
+        builder: (context, state) {
+          if (state is CartLoadedState) {
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: state.items.length,
+                    itemBuilder: (context, index) => CartItemWidget(
+                      cartItem: state.items[index],
+                    ),
+                  ),
+                ),
+                // CheckoutSummary(),
+              ],
+            );
+          }
+          return const Center(child: Text("Cart is empty or loading..."));
+        },
       ),
     );
   }
