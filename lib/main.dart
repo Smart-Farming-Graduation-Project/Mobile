7bedd22d48bf4c59ec5780b81218599f1b4ad9e1
Ali gad <aligad.17611@gmail.com>
2025-02-14 21:06:26 +0200

    change theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:crop_guard/core/utils/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -24,6 +25,7 @@ class CropGaurd extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'CropGaurd',
           theme: ThemeData(
+            primarySwatch: Colors.green,
             textTheme: GoogleFonts.robotoTextTheme(),
           ),
         );
