9f80ede6da7bd0dc0a2099bc7a9f26fb8a96299b
nohaahmed <nohaahmedk1@gmail.com>
2025-02-14 23:46:43 +0200

    some update in category

--- lib/featurs/categories/presentation/views/category_screen.dart
+++ lib/featurs/categories/presentation/views/category_screen.dart
@@ -1,8 +1,6 @@
-import 'package:crop_guard/core/utils/routing/app_router.dart';
 import 'package:crop_guard/featurs/home/presentation/views/widgets/category_button.dart';
 import 'package:crop_guard/featurs/home/presentation/views/widgets/item_card.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import '../../../home/presentation/models/recommended_model.dart';
 
 class CategoryScreen extends StatelessWidget {
