0a1543e40040c903add586283b79e944fb04b4f5
Ali gad <aligad.17611@gmail.com>
2025-02-18 09:21:31 +0200

    merge main branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:crop_guard/core/utils/theme/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
