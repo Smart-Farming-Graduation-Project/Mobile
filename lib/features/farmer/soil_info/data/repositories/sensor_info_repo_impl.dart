fbb4335b801c1144f95b2fe288174854a39afa0d
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-06-30 00:30:20 +0300

    feat: Integrate new soil data visualization features and improve UI components
    - Added new soil data visualization features to enhance user interaction.
    - Updated SoilInfoView and related components for better layout and usability.
    - Enhanced SoilChart to accurately display soil data trends over the last 7 days.
    - Improved styling and spacing in SoilQualityCard for better readability and presentation.

---
+++ lib/features/farmer/soil_info/data/repositories/sensor_info_repo_impl.dart
@@ -0,0 +1,41 @@
+import 'package:crop_guard/core/errors/exceptions.dart';
+import 'package:crop_guard/core/failure/failure_model.dart';
+import 'package:crop_guard/features/farmer/soil_info/data/datasources/soil_info_remote_datasource.dart';
+import 'package:crop_guard/features/farmer/soil_info/domain/entities/soil_analysis_entity.dart';
+import 'package:crop_guard/features/farmer/soil_info/domain/entities/soil_chart_entity.dart';
+import 'package:crop_guard/features/farmer/soil_info/domain/repositories/sensor_info_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class SensorInfoRepoImpl implements SensorInfoRepo {
+  final SoilInfoRemoteDataSource remoteDataSource;
+
+  SensorInfoRepoImpl({required this.remoteDataSource});
+
+  @override
+  Future<Either<FailureModel, SoilAnalysisEntity>> getSoilAnalysisData() async {
+    try {
+      final soilAnalysis = await remoteDataSource.getSoilAnalysis();
+      return Right(soilAnalysis);
+    } on ServerException catch (e) {
+      return Left(FailureModel(message: e.errorModel.errorMessage));
+    } on NetworkException catch (e) {
+      return Left(FailureModel(message: e.message));
+    } catch (e) {
+      return Left(FailureModel(message: e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<FailureModel, List<SoilChartEntity>>> getSoilChartData() async {
+    try {
+      final soilChart = await remoteDataSource.getChartData();
+      return Right(soilChart);
+    } on ServerException catch (e) {
+      return Left(FailureModel(message: e.errorModel.errorMessage));
+    } on NetworkException catch (e) {
+      return Left(FailureModel(message: e.message));
+    } catch (e) {
+      return Left(FailureModel(message: e.toString()));
+    }
+  }
+} 
