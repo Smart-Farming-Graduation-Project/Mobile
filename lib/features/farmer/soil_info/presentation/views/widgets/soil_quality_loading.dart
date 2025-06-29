f8fafca7f810308d8e26002e0592cc1368493e93
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-06-30 02:38:53 +0300

    fix bug

--- lib/features/farmer/soil_info/presentation/views/widgets/soil_quality_loading.dart
+++ lib/features/farmer/soil_info/presentation/views/widgets/soil_quality_loading.dart
@@ -1,7 +1,6 @@
 import 'package:crop_guard/core/helper/spacing.dart';
 import 'package:crop_guard/core/theme/app_colors.dart';
 import 'package:crop_guard/core/theme/app_text_styles.dart';
-import 'package:crop_guard/core/widgets/shimmer_effect.dart';
 import 'package:crop_guard/features/farmer/soil_info/presentation/views/widgets/soil_chart_skeleton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
