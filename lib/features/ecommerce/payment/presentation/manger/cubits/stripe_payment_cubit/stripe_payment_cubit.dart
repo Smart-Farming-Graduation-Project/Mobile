f2e010cdd134e92ec7a9ef88451f9207aa364597
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-07-05 01:31:39 +0300

    feat: Implement StripePaymentCubit and state management for payment processing
    - Added StripePaymentCubit to handle payment logic and state transitions.
    - Created StripePaymentState classes to represent different payment states: initial, loading, success, and failure.
    - Integrated payment intent handling through the CheckoutRepo for streamlined payment processing.

---
+++ lib/features/ecommerce/payment/presentation/manger/cubits/stripe_payment_cubit/stripe_payment_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:crop_guard/features/ecommerce/payment/data/models/payment_intent_input_model.dart';
+import 'package:crop_guard/features/ecommerce/payment/data/repos/checkout_repo.dart';
+import 'package:crop_guard/features/ecommerce/payment/presentation/manger/cubits/stripe_payment_cubit/stripe_payment_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class StripePaymentCubit extends Cubit<StripePaymentState> {
+  final CheckoutRepo checkoutRepo;
+
+  StripePaymentCubit({required this.checkoutRepo}) : super(StripePaymentInitial());
+
+  Future<void> makePayment({required PaymentIntentInputModel input}) async {
+    emit(StripePaymentLoading());
+    final result = await checkoutRepo.makePayment(input: input);
+    result.fold(
+      (failure) => emit(StripePaymentFailure(message: failure.message)),
+      (success) => emit(StripePaymentSuccess()),
+    );
+  }
+}
